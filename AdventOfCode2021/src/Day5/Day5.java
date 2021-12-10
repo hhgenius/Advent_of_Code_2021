1814dc5edf67df0b21f0741683bbfabbe5147858
hhgenius <36898623+hhgenius@users.noreply.github.com>
2021-12-10 19:11:04 +0100

    Update Day5.java

--- AdventOfCode2021/src/Day5/Day5.java
+++ AdventOfCode2021/src/Day5/Day5.java
@@ -145,12 +145,12 @@ public class Day5 {
         int resultado = 0;
         for(int i = 0; i<matrix.length; i++){
             for(int j = 0; j<matrix[i].length; j++){
-                //System.out.print(matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
                 if(matrix[i][j] >= 2){
                     resultado += 1;
                 }
             }
-            //System.out.println("");
+            System.out.println("");
         }
         return resultado;
     }
